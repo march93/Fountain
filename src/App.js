db3a3aa5bd276c23d52e32f4032b06044eba001f
Marc Ho <marc.ho380@gmail.com>
2017-10-14 22:15:38 -0700

    Added bottom navigation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
+import FontIcon from 'material-ui/FontIcon';
+import Paper from 'material-ui/Paper';
+import IconList from 'material-ui/svg-icons/action/list';
+import IconFavorite from 'material-ui/svg-icons/action/favorite';
+import IconLocationOn from 'material-ui/svg-icons/communication/location-on';
+
+const listIcon = <IconList />;
+const favoriteIcon = <IconFavorite />;
+const mapIcon = <IconLocationOn />;
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    state = {
+        selectedIndex: 0,
+    };
+
+    select = (index) => this.setState({selectedIndex: index});
+
+    render() {
+        return (
+        <Paper zDepth={1} className="paper">
+            <BottomNavigation selectedIndex={this.state.selectedIndex}>
+            <BottomNavigationItem
+                label="List"
+                icon={listIcon}
+                onClick={() => this.select(0)}
+            />
+            <BottomNavigationItem
+                label="Favorites"
+                icon={favoriteIcon}
+                onClick={() => this.select(1)}
+            />
+            <BottomNavigationItem
+                label="Map"
+                icon={mapIcon}
+                onClick={() => this.select(2)}
+            />
+            </BottomNavigation>
+        </Paper>
+        );
+    }
 }
 
 export default App;
